b059b1680604ec0cfb6dcf5fdf291f28b06d65b5
Murat <info.muratmehmet@gmail.com>
2025-01-26 23:59:33 +0200

    fix: update typings

--- lib/MOP.ts
+++ lib/MOP.ts
@@ -6,7 +6,7 @@ import {ObservablePromise, ObservablePromiseOptions, PromiseAction} from "./Obse
 
 export function MOP<T extends PromiseAction>(action: T, options?: ObservablePromiseOptions<T>): ObservablePromise<T>
 export function MOP<T, M extends keyof Methods<T>>(object: T, method: M, options?: ObservablePromiseOptions<PromiseMethod<T, M>>): CallPromise<T, M>
-export function MOP(...args: [PromiseAction, object?] | [object, string, object?] | [object?, object?]): any {
+export function MOP(...args: [PromiseAction, object?] | [object, string, object?]): any {
     if (typeof args[0] === 'function') {
         // @ts-ignore
         return new ObservablePromise(...args);
@@ -16,8 +16,8 @@ export function MOP(...args: [PromiseAction, object?] | [object, string, object?
     }
 }
 
-function infinite<T extends PromiseAction, TItem>(action: T, resolver: PageResolver<T, TItem>, options: ObservablePromiseOptions<T>): InfiniteObservablePromise<T, TItem>
-function infinite<T, M extends keyof Methods<T>, TItem>(object: T, method: M, resolver: PageResolver<PromiseMethod<T, M>, TItem>, options: ObservablePromiseOptions<PromiseMethod<T, M>>): InfiniteCallPromise<T, M, TItem>
+function infinite<T, M extends keyof Methods<T>, TItem>(object: T, method: M, resolver: PageResolver<PromiseMethod<T, M>, TItem>, options?: ObservablePromiseOptions<PromiseMethod<T, M>>): InfiniteCallPromise<T, M, TItem>
+function infinite<T extends PromiseAction, TItem>(action: T, resolver: PageResolver<T, TItem>, options?: ObservablePromiseOptions<T>): InfiniteObservablePromise<T, TItem>
 function infinite(...args: [PromiseAction, object, object?] | [object, string, object, object?]): any {
     if (typeof args[0] === 'function') {
         // @ts-ignore
